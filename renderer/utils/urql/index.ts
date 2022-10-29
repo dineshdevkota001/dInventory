667159d5883d73de9b1a10b7f6e485b9387b11e1
dineshdevkota001 <dineshdevkota001@gmail.com>
2022-10-30 01:21:07 +0545

    fix introspection

--- renderer/utils/urql/index.ts
+++ renderer/utils/urql/index.ts
@@ -2,10 +2,15 @@ import { BACKEND } from '@constants/static/api';
 import { cacheExchange } from '@urql/exchange-graphcache';
 import { createClient, dedupExchange, fetchExchange } from 'urql';
 import schema from '@generated/introspection.json';
+import { IntrospectionQuery } from 'graphql';
 
 const client = createClient({
   url: BACKEND,
-  exchanges: [dedupExchange, cacheExchange({ schema }), fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange({ schema: schema as unknown as IntrospectionQuery }),
+    fetchExchange,
+  ],
 });
 
 export default client;
